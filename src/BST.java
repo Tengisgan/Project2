6b273c67f1c8b75d2df3a2c4abe42347b0995eee
Tengis Gantulga <tengisgan@vt.edu>
2020-10-17 17:27:16 -0400

    some changes to bst

--- src/BST.java
+++ src/BST.java
@@ -20,3 +20,146 @@
 // Names: Tengis Gantulga and Nikolai Long
 // PIDs: tengisgan and nikolai
 
+
+public class BST {
+    //Node containing data and ability to go left and right
+    class Node {
+        String[] data;
+        Node left;
+        Node right;
+        
+        public Node(String[] data) {
+            this.data = data;
+            left = null;
+            right = null;
+            
+        }
+        
+    }
+    
+    // Fields
+    private Node root;
+    private int nodes;
+    
+    //Constructor for an empty tree
+    public BST() {
+        this.root = null;
+        this.nodes = 0;
+    }
+    
+    //make empty
+    public void makeEmpty() {
+        root = null;
+    }
+    
+    //checking for empty
+    public boolean isEmpty() {
+       this.nodes = 0;
+       return (root == null);
+    }
+    
+    //insertHelper is a helper method to insert
+    public Node insertHelper(String[] data, Node root) {
+        //if it is empty, simply make the root the new data and return it
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+        if (data[1].compareTo(root.data[1]) == 0) {
+            if ((data[0]).compareTo(root.data[0]) < 0) {
+                root.left = insertHelper(data, root.left);
+            }
+            //or to the right
+            else if ((data[0]).compareTo(root.data[0]) > 0){
+                root.right = insertHelper(data, root.right);
+            }
+        }
+        //checking if it is to the left
+        else if ((data[1]).compareTo(root.data[1]) < 0) {
+            root.left = insertHelper(data, root.left);
+        }
+        //or to the right
+        else if ((data[1]).compareTo(root.data[1]) > 0){
+            root.right = insertHelper(data, root.right);
+        }
+        return root;
+    }
+    
+    //Insert to a tree using the helper insertRecursion method
+    public void insert(String[] data) {
+        root = insertHelper(data, root);
+        this.nodes++;
+    }
+    
+//    //Finding the minimum value
+//    public String[] findMinValue(Node root) {
+//        Node current = root;
+//        
+//        while (current.left != null) {
+//            current = current.left;
+//        }
+//        return current.data;
+//    }
+//    
+//    public String[] findMinValue() {
+//        return findMinValue(root);
+//    }
+
+//    //helper method to delete
+//    public Node deleteHelper(String[] data, Node root) {
+//        
+//        if (root == null) {
+//            return root;
+//        }
+//        
+//        else if (data.compareTo(root.data) > 0) {
+//            root.right = deleteHelper(data, root.right);
+//        }
+//        else if (data.compareTo(root.data) < 0) {
+//            root.left = deleteHelper(data, root.left);
+//        }
+//        
+//        else {
+//            if (root.right == null) {
+//                return root.left;
+//            }
+//            else if (root.left == null) {
+//                return root.right;
+//            }
+//            
+//            root.data = findMinValue(root.right);
+//            root.right = deleteHelper(root.data, root.right);
+//        }
+//        
+//        return root;
+//    }
+    
+    ////delete method
+//    public void delete(String data) {
+//        root = deleteHelper(data, root);
+//        this.nodes--;
+//    }
+//    
+    //finding method (internal)
+    public Node findHelper(String[] data, Node root) {
+        
+        if (root == null || (root.data[0] == data[0] && root.data[1] == data[1])) {
+            return root;
+        }
+        //checking if it is to the left
+        if (data[1].compareTo(root.data[1]) < 0 && data[0].compareTo(root.data[0]) < 0) {
+            root.left = findHelper(data, root.left);
+        }
+        return findHelper(data, root.right);
+    }
+    
+   
+//    public boolean find(String[] data) {
+//        return findHelper(data, root);
+//    }
+
+    //getting the size
+    public int getSize() {
+        return this.nodes; 
+    }
+}
--- src/Tests.java
+++ src/Tests.java
@@ -21,4 +21,42 @@
 // PIDs: tengisgan and nikolai
 
 
+import java.io.FileNotFoundException;
+import student.TestCase;
 
+/**
+ * Test class for project 1. 
+ * 
+ * @author Tengis Gantulga and Nikolai Long
+
+ * 
+ * @version 2020.09.29
+ */
+public class Tests extends TestCase {
+    
+    private BST bst; 
+
+    
+    public void setUp() {
+        bst = new BST();
+    }
+    
+    public void testInsert() {
+        String[] firstData = {"20200724", "OK",  "442", "40",  "335", "195", "175", "377", "D+",  "343"};
+        String[] secondData = {"20200706",  "VT",  "772", "876", "579", "836", "987", "744", "F",   "0"};
+        
+        bst.insert(firstData);
+        assertEquals(bst.getSize(), 1);
+        bst.insert(secondData);
+        assertEquals(bst.getSize(), 2);
+        
+    }
+    
+    public void testFind() {
+        String[] firstData = {"20200724", "OK",  "442", "40",  "335", "195", "175", "377", "D+",  "343"};
+        String[] secondData = {"20200706",  "VT",  "772", "876", "579", "836", "987", "744", "F",   "0"};
+        bst.insert(firstData);
+        assertNull(bst.find(secondData));
+    }
+    
+}
