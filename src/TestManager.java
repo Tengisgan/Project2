c9d95de1dd7f3cb95911bc2a947e7e8f0301c0d2
nikolai <nikolai@vt.edu>
2020-10-17 17:37:39 -0400

    fixed conflicts

--- src/TestManager.java
+++ src/TestManager.java
@@ -23,7 +23,6 @@
 import java.io.FileNotFoundException;
 import student.TestCase;
 
-<<<<<<< HEAD:src/TestManager.java
 /* Test class for Covid19TrackingManager2
  * 
  * @author Tengis Gantulga and Nikolai Long
@@ -57,45 +56,4 @@ public class TestManager extends TestCase {
     }
 
 
-}
-=======
-import java.io.FileNotFoundException;
-import student.TestCase;
->>>>>>> 62a6d1c5965dc3cf00692f3243b279086b1adc89:src/Tests.java
-
-/**
- * Test class for project 1. 
- * 
- * @author Tengis Gantulga and Nikolai Long
-
- * 
- * @version 2020.09.29
- */
-public class Tests extends TestCase {
-    
-    private BST bst; 
-
-    
-    public void setUp() {
-        bst = new BST();
-    }
-    
-    public void testInsert() {
-        String[] firstData = {"20200724", "OK",  "442", "40",  "335", "195", "175", "377", "D+",  "343"};
-        String[] secondData = {"20200706",  "VT",  "772", "876", "579", "836", "987", "744", "F",   "0"};
-        
-        bst.insert(firstData);
-        assertEquals(bst.getSize(), 1);
-        bst.insert(secondData);
-        assertEquals(bst.getSize(), 2);
-        
-    }
-    
-    public void testFind() {
-        String[] firstData = {"20200724", "OK",  "442", "40",  "335", "195", "175", "377", "D+",  "343"};
-        String[] secondData = {"20200706",  "VT",  "772", "876", "579", "836", "987", "744", "F",   "0"};
-        bst.insert(firstData);
-        assertNull(bst.find(secondData));
-    }
-    
 }
