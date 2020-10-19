5a0355ab181c6541ba5a885cdb0e82a44420e3ab
nikolai <nikolai@vt.edu>
2020-10-19 17:19:14 -0400

    new BST class TENGIS YOU BETTER NOT MAKE A NEW CLASS

--- src/BST.java
+++ src/BST.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 // On my honor:
 //
 // - I have not used source code obtained from another student,
@@ -20,146 +22,142 @@
 // Names: Tengis Gantulga and Nikolai Long
 // PIDs: tengisgan and nikolai
 
-
+/**
+ * BST implementation including: TreeNode, Data, and Flyweight
+ * 
+ * @author Tengis Gantulga and Nikolai Long
+ * @pid tengisgan and nikolai
+ * @version 2020.10.12
+ */
 public class BST {
-    //Node containing data and ability to go left and right
-    class Node {
-        String[] data;
-        Node left;
-        Node right;
-        
-        public Node(String[] data) {
-            this.data = data;
-            left = null;
-            right = null;
-            
-        }
-        
-    }
-    
-    // Fields
-    private Node root;
-    private int nodes;
-    
-    //Constructor for an empty tree
-    public BST() {
-        this.root = null;
-        this.nodes = 0;
-    }
-    
-    //make empty
-    public void makeEmpty() {
-        root = null;
-    }
-    
-    //checking for empty
-    public boolean isEmpty() {
-       this.nodes = 0;
-       return (root == null);
-    }
-    
-    //insertHelper is a helper method to insert
-    public Node insertHelper(String[] data, Node root) {
-        //if it is empty, simply make the root the new data and return it
-        if (root == null) {
-            root = new Node(data);
-            return root;
-        }
-        if (data[1].compareTo(root.data[1]) == 0) {
-            if ((data[0]).compareTo(root.data[0]) < 0) {
-                root.left = insertHelper(data, root.left);
-            }
-            //or to the right
-            else if ((data[0]).compareTo(root.data[0]) > 0){
-                root.right = insertHelper(data, root.right);
-            }
-        }
-        //checking if it is to the left
-        else if ((data[1]).compareTo(root.data[1]) < 0) {
-            root.left = insertHelper(data, root.left);
-        }
-        //or to the right
-        else if ((data[1]).compareTo(root.data[1]) > 0){
-            root.right = insertHelper(data, root.right);
-        }
-        return root;
-    }
-    
-    //Insert to a tree using the helper insertRecursion method
-    public void insert(String[] data) {
-        root = insertHelper(data, root);
-        this.nodes++;
-    }
-    
-//    //Finding the minimum value
-//    public String[] findMinValue(Node root) {
-//        Node current = root;
-//        
-//        while (current.left != null) {
-//            current = current.left;
-//        }
-//        return current.data;
-//    }
-//    
-//    public String[] findMinValue() {
-//        return findMinValue(root);
-//    }
-
-//    //helper method to delete
-//    public Node deleteHelper(String[] data, Node root) {
-//        
-//        if (root == null) {
-//            return root;
-//        }
-//        
-//        else if (data.compareTo(root.data) > 0) {
-//            root.right = deleteHelper(data, root.right);
-//        }
-//        else if (data.compareTo(root.data) < 0) {
-//            root.left = deleteHelper(data, root.left);
-//        }
-//        
-//        else {
-//            if (root.right == null) {
-//                return root.left;
-//            }
-//            else if (root.left == null) {
-//                return root.right;
-//            }
-//            
-//            root.data = findMinValue(root.right);
-//            root.right = deleteHelper(root.data, root.right);
-//        }
-//        
-//        return root;
-//    }
-    
-    ////delete method
-//    public void delete(String data) {
-//        root = deleteHelper(data, root);
-//        this.nodes--;
-//    }
-//    
-    //finding method (internal)
-    public Node findHelper(String[] data, Node root) {
-        
-        if (root == null || (root.data[0] == data[0] && root.data[1] == data[1])) {
-            return root;
-        }
-        //checking if it is to the left
-        if (data[1].compareTo(root.data[1]) < 0 && data[0].compareTo(root.data[0]) < 0) {
-            root.left = findHelper(data, root.left);
-        }
-        return findHelper(data, root.right);
-    }
-    
-   
-//    public boolean find(String[] data) {
-//        return findHelper(data, root);
-//    }
-
-    //getting the size
-    public int getSize() {
-        return this.nodes; 
-    }
+	public class TreeNode {
+		TreeNode left;
+		TreeNode right;
+		String state;
+		ArrayList<Data> data;
+		
+		public TreeNode() {}
+		
+		public TreeNode(String[] data) {
+			this.left = fly;
+			this.right = fly;
+			state = data[1];
+			this.data = new ArrayList<Data>();
+		}
+		
+		public void sortTreeNode() {
+			for (int i = 1; i < this.data.size(); i++) {
+				Data current = this.data.get(i);
+				for (int j = 0; j < i; j++) {
+					if (current.data[0].compareTo(this.data.get(j).data[0]) > 0) {
+						Data placeHolder = this.data.get(j);
+						this.data.set(j, current);
+						this.data.set(i, placeHolder);
+					}
+				}
+			}
+		}
+	}
+	
+	public class Data extends TreeNode {
+		String[] data;
+		
+		public Data(String[] data) {
+			this.data = data;
+		}
+	}
+	
+	public class Flyweight extends TreeNode {
+		TreeNode left = null;
+		TreeNode right = null;
+		String state = null;
+		Data[] data = null;
+		
+		public Flyweight() {
+			
+		}
+	}
+	
+	/**
+	 * BST Fields
+	 */
+	private TreeNode root;
+	private Flyweight fly;
+    private int numNodes;
+	
+	public BST() {
+		fly = new Flyweight();
+		root = fly;
+		numNodes = 0;
+	}
+	
+	public TreeNode find(String state, TreeNode node) {
+		if (state.equals(node.state) || (node.left instanceof Flyweight && node.right instanceof Flyweight)) {
+			return node;
+		}
+		else if (state.compareTo(node.state) < 0) {
+			return this.find(state, node.left);
+		}
+		return this.find(state, node.right);
+	}
+	
+	public void insert(String[] data) {
+		TreeNode found = this.find(data[1], root);
+		if (found instanceof Flyweight) {
+			
+		}
+		else {
+			this.find(data[1], root);
+		}
+	}
+	
+	
+	
+	public boolean compareGrades() {
+		return false;
+	}
+	
+	public void replace() {
+		
+	}
+	
+	public boolean updateData() {
+		return false;
+	}
+	
+	public int removeGrade() {
+		return 0;
+	}
+	
+	public int sortedPrint() {
+		return 0;
+	}
+	
+	public String getLatestDate() {
+		return "";
+	}
+	
+	public void printState() {
+		
+	}
+	
+	public void printCases() {
+		
+	}
+	
+	public void printAverage() {
+		
+	}
+	
+	public void printNumber() {
+		
+	}
+	
+	public void printQuality() {
+		
+	}
+	
+	public void printDate() {
+		
+	}
 }
--- src/TestBST.java
+++ src/TestBST.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import student.TestCase;
+
 /**
  * Test class for project 1. 
  * 
@@ -17,22 +18,8 @@ public class TestBST extends TestCase {
         bst = new BST();
     }
     
-    public void testInsert() {
-        String[] firstData = {"20200724", "OK",  "442", "40",  "335", "195", "175", "377", "D+",  "343"};
-        String[] secondData = {"20200706",  "VT",  "772", "876", "579", "836", "987", "744", "F",   "0"};
-        
-        bst.insert(firstData);
-        assertEquals(bst.getSize(), 1);
-        bst.insert(secondData);
-        assertEquals(bst.getSize(), 2);
-        
-    }
-    
-    public void testFind() {
-        String[] firstData = {"20200724", "OK",  "442", "40",  "335", "195", "175", "377", "D+",  "343"};
-        String[] secondData = {"20200706",  "VT",  "772", "876", "579", "836", "987", "744", "F",   "0"};
-        bst.insert(firstData);
-        assertNull(bst.find(secondData));
+    public void testDataSort() {
+    	
     }
     
 }
